a274dfe524f34752484ab538ae4e8e7ee5caa3d7
Veselin Todorov <veskotodorov786@proton.me>
2025-04-18 21:41:52 +0300

    Add more advanced statistics

--- ShapesProject.UI/Forms/StatisticsForm.cs
+++ ShapesProject.UI/Forms/StatisticsForm.cs
@@ -6,6 +6,11 @@ public partial class StatisticsForm : Form
 {
     private readonly IEnumerable<Shape> _shapes;
     
+    private List<object> _topThreeLargest;
+    private List<object> _avgByType;
+    private List<string> _allAboveMin;
+    private string _mostCommonType;
+    
     public StatisticsForm(IEnumerable<Shape> shapes)
     {
         InitializeComponent();
@@ -36,5 +41,46 @@ public partial class StatisticsForm : Form
 
         labelTotalCount.Text = @$"Total Count: {totalCount}";
         labelTotalArea.Text = @$"Total Area: {totalArea:f2}";
+
+
+        //1
+        _topThreeLargest = _shapes
+            .Select(s => new
+            {
+                Type = s.GetType().Name,
+                Area = s.CalculateArea()
+            })
+            .OrderByDescending(s => s.Area)
+            .Take(3)
+            .ToList<object>();
+        
+        //2
+        _avgByType = _shapes
+            .Where(s => s.CalculateArea() > 1000)
+            .GroupBy(s => s.GetType().Name)
+            .Select(group => new
+            {
+                Type = group.Key,
+                AverageArea = Math.Round(group.Average(s => s.CalculateArea()), 2)
+            })
+            .OrderByDescending(x => x.AverageArea)
+            .ToList<object>();
+        
+        
+        //3
+        const double minArea = 5.0;
+        _allAboveMin = _shapes
+            .GroupBy(s => s.GetType().Name)
+            .Where(g => g.All(s => s.CalculateArea() > minArea))
+            .Select(s => s.GetType().Name)
+            .ToList();
+        
+        //4
+        _mostCommonType = _shapes
+            .GroupBy(s => s.GetType().Name)
+            .OrderByDescending(g => g.Count())
+            .Select(s => s.Key)
+            .FirstOrDefault()
+            ?? "None";
     }
 }
